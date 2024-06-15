b552313ca2a4b829d8ad12b0d5250a127709d46a
Danil Grishin <danil.grishin.99@inbox.ru>
2024-06-15 11:01:00 +1000

    2nd push on githab

--- web.py
+++ web.py
@@ -15,6 +15,7 @@ def index():
         return render_template('index.html')
 
 if __name__ == '__main__':
+
     app.run(host="0.0.0.0")
     # serve(app, port=5000)
     # serve(app, host='0.0.0.0', port=5000, url_scheme='https')
