1bdb2d37882ec6187c119fba3e0fa396cf333174
Danil Grishin <danil.grishin.99@inbox.ru>
2024-06-15 18:52:22 +1000

    3rd push on github

--- parser_tinkoff.py
+++ parser_tinkoff.py
@@ -8,7 +8,7 @@ def parser_tinkoff(start, end):
     regionName = ['all', 'Амурская область', 'Камчатский край', 'Приморский край',
                   'Республика Саха (Якутия)', 'Сахалинская область', 'Хабаровский край']
 
-    # writer = pd.ExcelWriter('tinkoff_index.xlsx', engine='openpyxl', if_sheet_exists='replace', mode='a')
+    writer = pd.ExcelWriter('tinkoff_index.xlsx', engine='openpyxl', if_sheet_exists='replace', mode='a')
 
     for reg in regionName:
         print(reg)
@@ -23,5 +23,5 @@ def parser_tinkoff(start, end):
     #
     #     df.to_excel(writer, sheet_name=reg)
     #
-    # writer.close()
+    writer.close()
     # #writer.save()
