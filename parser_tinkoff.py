ec98f3764e74d118fa762f1f6f6a18632ad3eff6
Danil Grishin <danil.grishin.99@inbox.ru>
2024-06-16 10:57:56 +1000

    5th push on github

--- parser_tinkoff.py
+++ parser_tinkoff.py
@@ -13,15 +13,30 @@ def parser_tinkoff(start, end):
     for reg in regionName:
         print(reg)
 
-    #     url = f'https://index.tinkoff.ru/corona-index/papi/period_region_total?regionName={reg}&start={start}&end={end}'
-    #
-    #     r = requests.get(url)
-    #
-    #     df = pd.DataFrame(r.json()['consumerTotalPoints'])
-    #
-    #     # print(df)
-    #
-    #     df.to_excel(writer, sheet_name=reg)
-    #
+        url = f'https://index.tinkoff.ru/corona-index/papi/period_region_total?regionName={reg}&start={start}&end={end}'
+
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15',
+            'Accept': 'application/json',
+            'Connection': 'keep-alive',
+            'Accept-Encoding': 'gzip, deflate, br'
+        }
+
+        # http_proxy = "http://ip:port"
+        # https_proxy = "https://ip:port"
+
+        # proxyDict = {
+        #     # "http": http_proxy,
+        #     "https": https_proxy
+        # }
+
+        r = requests.get(url, headers=headers) # , proxies=proxyDict
+
+        df = pd.DataFrame(r.json()['consumerTotalPoints'])
+
+        # print(df)
+
+        df.to_excel(writer, sheet_name=reg)
+
     writer.close()
     # #writer.save()
